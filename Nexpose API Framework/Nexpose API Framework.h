0dbf4f3274185580195ab2a2d24bdf07fe732667
Joseph Pesce <joseph@josephs-air.urmc-sh.rochester.edu>
2017-05-18 13:12:49 -0400

    Initial Commit

---
+++ Nexpose API Framework/Nexpose API Framework.h
@@ -0,0 +1,19 @@
+//
+//  Nexpose API Framework.h
+//  Nexpose API Framework
+//
+//  Created by Joseph Pesce on 5/18/17.
+//  Copyright © 2017 Joseph Pesce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Nexpose API Framework.
+FOUNDATION_EXPORT double Nexpose_API_FrameworkVersionNumber;
+
+//! Project version string for Nexpose API Framework.
+FOUNDATION_EXPORT const unsigned char Nexpose_API_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nexpose_API_Framework/PublicHeader.h>
+
+
